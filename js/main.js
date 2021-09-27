df04d58d640b05ddeb54d55c40f31cbd9d5b4987
Mebin K Thomas <mebinkthomas@gmail.com>
2021-09-27 14:39:00 +0530

    removed mapbox

--- js/main.js
+++ js/main.js
@@ -44,12 +44,3 @@ const swiperProducts = new Swiper('.swiper2', {
   },
 });
 
-
-mapboxgl.accessToken = 'pk.eyJ1IjoibWViaW5rdGhvbWFzIiwiYSI6ImNrdHNjNzl6MzFlenUyeGw0YjczamdxbzIifQ.v4MxUWr_UU1aiZpa_pJ3kQ';
-  let map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
-  center: [-74.5, 40],
-  zoom: 8
-
-});
